5d63df67375045b931c986b8d494d889e2725d54
nitekat1124 <nitekat1124@gmail.com>
2021-12-13 22:22:00 +0800

    Day 13 improved

--- solutions/day13.py
+++ solutions/day13.py
@@ -25,6 +25,8 @@ class Solution(SolutionBase):
             test_counter += 1
         print()
 
+    """
+    # original
     def parse_data(self, data):
         sep = data.index("")
 
@@ -77,3 +79,38 @@ class Solution(SolutionBase):
             print(*["#" if c == "#" else " " for c in i], sep="")
         print()
         return 0
+    """
+
+    # improved
+    def parse_data(self, data):
+        sep = data.index("")
+        dots = [(*map(int, i.split(",")),) for i in data[:sep]]
+        instructions = [[(x := i.split()[2].split("="))[0], int(x[1])] for i in data[sep + 1 :]]
+        return dots, instructions
+
+    def fold(self, dots, instructions):
+        for inst in instructions:
+            w = max(i[0] for i in dots)
+            h = max(i[1] for i in dots)
+            inst_type = int(inst[0] == "y")
+            leng = [w, h][inst_type]
+            padding = inst[1] * 2 - leng
+            fold_after_half = leng / 2 < inst[1]
+            for i, v in enumerate(dots):
+                x, y = v
+                p = [x, y][inst_type]
+                p = [[padding + p, leng - p], [p, 2 * inst[1] - p]][fold_after_half][p > inst[1]]
+                dots[i] = [(p, y), (x, p)][inst_type]
+        return [*set(dots)]
+
+    def part1(self, data):
+        dots, instructions = self.parse_data(data)
+        dots = self.fold(dots, instructions[:1])
+        return len(dots)
+
+    def part2(self, data):
+        dots, instructions = self.parse_data(data)
+        dots = self.fold(dots, instructions)
+        w = max(i[0] for i in dots) + 1
+        h = max(i[1] for i in dots) + 1
+        print(*["".join("#" if (i, j) in dots else " " for i in range(w)) for j in range(h)], sep="\n")
