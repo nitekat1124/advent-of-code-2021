6f7f52b61059cf6d18d378c9a589844e0a0dd97f
nitekat1124 <nitekat1124@gmail.com>
2021-12-21 17:09:44 +0800

    improve puzzle reader

--- utils/puzzle_reader.py
+++ utils/puzzle_reader.py
@@ -4,12 +4,12 @@ import os, sys, glob, re
 class PuzzleReader:
     @staticmethod
     def get_puzzle_input(day_num):
-        return [line.strip() for line in open(f"{os.path.dirname(os.path.realpath(sys.argv[0]))}/data/day{day_num:02d}/puzzle_input.txt", "r").readlines()]
+        return [line.strip() for line in open(f"{PuzzleReader.get_path()}/data/day{day_num:02d}/puzzle_input.txt", "r").readlines()]
 
     @staticmethod
     def get_test_input(day_num):
         inputs = []
-        for name in sorted(glob.glob(f"{os.path.dirname(os.path.realpath(sys.argv[0]))}/data/day{day_num:02d}/*")):
+        for name in sorted(glob.glob(f"{PuzzleReader.get_path()}/data/day{day_num:02d}/*")):
             if len(re.findall(r"^test_\d+_input.txt$", os.path.basename(name))):
                 inputs += [[line.strip() for line in open(name, "r").readlines()]]
         return inputs
@@ -17,7 +17,11 @@ class PuzzleReader:
     @staticmethod
     def get_test_result(day_num, part_num):
         results = []
-        for name in sorted(glob.glob(f"{os.path.dirname(os.path.realpath(sys.argv[0]))}/data/day{day_num:02d}/*")):
+        for name in sorted(glob.glob(f"{PuzzleReader.get_path()}/data/day{day_num:02d}/*")):
             if len(re.findall(r"^test_\d+_part" + str(part_num) + "_result.txt$", os.path.basename(name))):
                 results += [[line.strip() for line in open(name, "r").readlines()]]
         return results
+
+    @staticmethod
+    def get_path():
+        return path if os.path.isdir(path := os.path.realpath(sys.argv[0])) else os.path.dirname(path)
